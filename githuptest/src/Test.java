928f433bfc1535902a6cfc1ae6039feb7cdb61fe
yu-guanming <2510047791@qq.com>
2025-01-01 11:10:20 +0800

    测试提交代码到githup

---
+++ githuptest/src/Test.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 25100
+ * Date: 2025-01-01
+ * Time: 11:09
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello githup");
+    }
+}
